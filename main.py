701db76c3b7257c597a6ccb7c464bb335e48584b
Priyam Sharma <sharmapriyam05@gmail.com>
2026-09-14 17:43:41 +0530

    Using Tavily's Langgraph implementation as search tool

--- main.py
+++ main.py
@@ -3,24 +3,26 @@ from langchain.agents import create_agent
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from tavily import TavilyClient
+# from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
 load_dotenv()
-tavily = TavilyClient()
+# tavily = TavilyClient()
 
-@tool
-def search_tool(query: str) -> str:
-    """Tools that search the web for relevant information.
-    Args:
-        query (str): The query to search for.
-    Returns:
-        str: The search result
-    """
-    print(f"Searching for {query}")
-    return tavily.search(query=query)
+# @tool
+# def search_tool(query: str) -> str:
+#     """Tools that search the web for relevant information.
+#     Args:
+#         query (str): The query to search for.
+#     Returns:
+#         str: The search result
+#     """
+#     print(f"Searching for {query}")
+#     return tavily.search(query=query)
 
 llm = ChatOpenAI(model="gpt-4o",temperature=0)
-tools = [search_tool]
+# tools = [search_tool]
+tools = [TavilySearch()]
 agent = create_agent(llm, tools=tools)
 
 
